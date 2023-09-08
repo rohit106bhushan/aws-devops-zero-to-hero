e9387d8cd927a7b6890cc77a88711634434af248
rohit106bhushan <58414896+rohit106bhushan@users.noreply.github.com>
2023-09-08 23:31:08 +0530

    Update start_container.sh
    added rooot privelegs

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull rohit106bhushan/simple-python-flask-app
+sudo docker pull rohit106bhushan/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 rohit106bhushan/simple-python-flask-app
+sudo docker run -d -p 5000:5000 rohit106bhushan/simple-python-flask-app
